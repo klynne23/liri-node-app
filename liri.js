f865e8834ac7904e80307367865f2088a20fa7dd
Kirthi Bisset <kirthi.bisset@yahoo.com>
2019-01-03 13:12:40 -0500

    spotify api working

--- liri.js
+++ liri.js
@@ -1,2 +1,25 @@
 require("dotenv").config();
+var axios = require("axios");
+var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+// console.log(spotify);
+
+
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  
+  console.log(data); 
+  });
+  
+
+
+// const spotifyUrl = "https://api.spotify.com/v1/search?q=roadhouse%20blues"
+
+// axios.get(spotifyUrl).then(function(response){
+//     console.log(response);
+// });
