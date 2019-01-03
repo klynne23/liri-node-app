0a0652b1a12ced605f3012cbe3bac28652fe7ea4
Kirthi Bisset <kirthi.bisset@yahoo.com>
2019-01-03 15:58:13 -0500

    update to files

--- liri.js
+++ liri.js
@@ -5,21 +5,78 @@ var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
-// console.log(spotify);
+var userSelection = process.argv[2];
+var input = process.argv[3];
 
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-  
-  console.log(data); 
-  });
-  
+var spotifySearch = (input) => {
+
+    spotify.search({ type: 'track', query: input }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        //   console.log(data); 
+
+        var item = data.tracks.items[0]
+
+        console.log("--------------------------")
+        console.log(item.name);
+        console.log(item.preview_url);
+        // if item.preview_url == "null", display "no preview available "
+        console.log(item.artists[0].name);
+        console.log(item.album.name);
+
+    }); // end spotify search
 
 
-// const spotifyUrl = "https://api.spotify.com/v1/search?q=roadhouse%20blues"
+}    // end spotifySearch function
 
-// axios.get(spotifyUrl).then(function(response){
-//     console.log(response);
-// });
+var concertSearch = (input) => {
+
+    var queryURL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
+    axios.get(queryURL).then(function(response){
+
+        var array = response.data;
+
+        array.forEach((item)=>{
+            console.log("----------------------------");
+            console.log(item.venue.name);
+            console.log(item.venue.city + ", " + item.venue.country);
+            console.log(item.datetime);
+        })
+        
+    }) // end .then
+
+} // end concertSearch
+
+var movieSearch = (input) => {
+
+    var queryURL = "https://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
+    axios.get(queryURL).then(function(response){
+        // console.log(response.data)
+        var item = response.data;
+        console.log("----------------------------");
+        console.log(item.Title);
+        console.log(item.Year);
+        console.log(item.imdbRating);
+        // imbd rating, rotten tomatoes rating, country, language, actors
+        console.log(item.Plot);
+    })
+
+} // end movieSearch
+
+
+switch (userSelection) {
+    case "spotify-this-song":
+        spotifySearch(input);
+        break;
+    case "concert-this":
+        concertSearch(input);
+        break;
+    case "movie-this":
+        movieSearch(input);
+        break;
+    case "do-what-it-says":
+        // fileSearch(input);
+        break;
+}
